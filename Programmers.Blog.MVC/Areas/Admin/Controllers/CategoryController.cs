fa8b4d58479a975ad99b24fa03067786ef147383
Berke <85216724+blorein-code@users.noreply.github.com>
2022-03-15 00:59:38 +0300

    silme işlemlerinde sweetalert çalışır hale getirildi, animasyonlu bir şekilde silme işlemi tamamlandı. Yeni kategori eklendiğinde düzenle ve sil butonları gelmiyor bu eklenecek. Geri kalan işlemler tamamlanıp altyapılar hazırlandı.

--- Programmers.Blog.MVC/Areas/Admin/Controllers/CategoryController.cs
+++ Programmers.Blog.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -24,7 +24,7 @@ namespace ProgrammersBlog.MVC.Areas.Admin.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var result = await _categoryService.GetAll();
+            var result = await _categoryService.GetAllByNonDeleted();
 
             return View(result.Data);
 
@@ -59,7 +59,7 @@ namespace ProgrammersBlog.MVC.Areas.Admin.Controllers
         }
         public async Task<JsonResult> GetAllCategories()
         {
-            var result = await _categoryService.GetAll();
+            var result = await _categoryService.GetAllByNonDeleted();
             var categories = JsonSerializer.Serialize(result.Data,new JsonSerializerOptions 
             {
                 ReferenceHandler = ReferenceHandler.Preserve
@@ -70,8 +70,8 @@ namespace ProgrammersBlog.MVC.Areas.Admin.Controllers
         public async Task<JsonResult> Delete (int categoryId)
         {
             var result = await _categoryService.Delete(categoryId, "Berke Topcu");
-            var ajaxResult = JsonSerializer.Serialize(result);
-            return Json(ajaxResult);
+            var deletedCategory = JsonSerializer.Serialize(result.Data);
+            return Json(deletedCategory);
         }
     }
 }
